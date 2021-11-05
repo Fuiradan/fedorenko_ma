c899241739a4fbb78901764ebef72c83d78d8153
Fuiradan <maxfedorenko86@mail.ru>
2021-11-05 23:19:34 +0500

    Lesson №4

--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,13 @@
 sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-], function (XMLView) {
+	"sap/ui/core/ComponentContainer"
+], function (ComponentContainer) {
 	"use strict";
 
-	XMLView.create({
-		viewName: "fedorenko_ma.webapp.view.App"
-	}).then(function (oView) {
-		oView.placeAt("content");
-	});
+	new ComponentContainer({
+		viewName: "fedorenko_ma.webapp",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
+	}).placeAt("content");
 });
